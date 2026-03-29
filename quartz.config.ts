75a6262c096cdd25d860b807db8e5bc8c0c2edc3
WizardRason <36075971+WizardRason@users.noreply.github.com>
2026-03-29 11:50:04 -0400

    Update quartz.config.ts

--- quartz.config.ts
+++ quartz.config.ts
@@ -8,7 +8,7 @@ import * as Plugin from "./quartz/plugins"
  */
 const config: QuartzConfig = {
   configuration: {
-    pageTitle: "The Wedding Party",
+    pageTitle: "Wedding Party",
     pageTitleSuffix: "",
     enableSPA: true,
     enablePopovers: true,
